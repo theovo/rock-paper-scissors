173e42431ae9da22bc14802a4167e699336c54f5
Theo <36752680+theovo@users.noreply.github.com>
2024-01-12 10:43:57 -0800

    Complete Question 4 and 5

--- script.js
+++ script.js
@@ -1,19 +1,93 @@
-function getRandomInt(min, max) {
+// function getComputerChoice(min, max) {
+
+//     min = Math.ceil(min); // makes min inclusive
+//     max = Math.floor(max); // makes max inclusive
+//     let randomInt = Math.floor(Math.random() * (max - min + 1)) + min;
+
+//     let a = "rock";
+//     let b = "paper";
+//     let c = "scissors";
+
+//     if (randomInt === 1) {
+//         return a;
+//     } else if (randomInt === 2) {
+//         return b;
+//     } else {
+//         return c;
+//     };
+
+// }
+
+// let computerSelection = (getComputerChoice(1,3));
+// console.log(computerSelection);
+
+function getComputerChoice(min, max) {
+
     min = Math.ceil(min); // makes min inclusive
     max = Math.floor(max); // makes max inclusive
-    let randomInt = Math.floor(Math.random() * (max - min + 1)) + min;
+    randomInt = Math.floor(Math.random() * (max - min + 1)) + min;
+    return randomInt;
 
-    let a = "Rock";
-    let b = "Paper";
-    let c = "Scissors";
-
-    if (randomInt === 1) {
-        return a;
-    } else if (randomInt === 2) {
-        return b;
-    } else {
-        return c;
-    };
 }
 
-console.log(getRandomInt(1,3));
+function getUserChoice() {
+
+    let userInput = prompt("Choose one of the following: rock paper scissors", "");
+    // console.log(userInput);
+
+    userInput = userInput.toLowerCase();
+    // console.log(userInput);
+
+    if (userInput === "rock") {
+        return (userInput = 1);
+    } else if (userInput === "paper") {
+        return (userInput = 2);
+    } else if (userInput === "scissors") {
+        return (userInput = 3);
+    } else {
+        return (userInput = null);
+    }
+
+}
+
+function playRound(playerSelection, computerSelection) {
+
+    //alert(`${playerSelection} ${computerSelection}`);
+
+    // convert to strings
+    if (computerSelection === 1) {
+        computerSelectionAsString = "Rock";
+    } else if (computerSelection === 2) {
+        computerSelectionAsString = "Paper";
+    } else {
+        computerSelectionAsString = "Scissors";
+    }
+
+    // convert to strings
+    if (playerSelection === 1) {
+        playerSelectionAsString = "Rock";
+    } else if (playerSelection === 2) {
+        playerSelectionAsString = "Paper";
+    } else if (playerSelection === 3) {
+        playerSelectionAsString = "Scissors";
+    }
+
+    // compare the selections, alert the results, return the winner of the round
+    if (playerSelection === computerSelection || playerSelection === null) {
+        alert("Try again!");
+        computerSelection = getComputerChoice(1,3);
+        playerSelection = getUserChoice();
+        console.log(playRound(playerSelection, computerSelection));
+    } else if (playerSelection < computerSelection) {
+        alert(`You lose! ${playerSelectionAsString} loses to ${computerSelectionAsString}!`);
+        return "computer";
+    } else {
+        alert(`You win! ${playerSelectionAsString} beats ${computerSelectionAsString}!`);
+        return "player";
+    }
+}
+
+const computerSelection = getComputerChoice(1,3);
+const playerSelection = getUserChoice();
+
+console.log(playRound(playerSelection, computerSelection));
